5f80af3bb9a0e9a9bd5cd4bec2709cfe22339f55
iamoki <pureaoh@gmail.com>
2022-07-27 15:41:30 +0900

    ✅ 라우터로 스탭구분

--- src/components/ActionButtons/index.js
+++ src/components/ActionButtons/index.js
@@ -1,12 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+
 function ActionButtons({ questionsLength, step }) {
   const isLast = questionsLength - 1 === step;
-
+  const navigate = useNavigate();
   return (
     <div>
       {/* 스텝넘버가 0이라면 이전버튼은 노출되지않고 0 다음 숫자일 경우 이전버튼 그리기 */}
-      {step === 0 || <button>이전</button>}
+      {step === 0 || (
+        <button
+          onClick={() => {
+            navigate(`${step - 1}`);
+          }}
+        >
+          이전
+        </button>
+      )}
       {/* isLast에있는 questionsLength가 마지막번호가 아니라면 다음버튼이 보여지고 마지막번호라면 제출번호 그리기 */}
-      {isLast ? <button>제출</button> : <button>다음</button>}
+      {isLast ? (
+        <button
+          onClick={() => {
+            navigate('/done');
+          }}
+        >
+          제출
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            navigate(`${step + 1}`);
+          }}
+        >
+          다음
+        </button>
+      )}
     </div>
   );
 }
